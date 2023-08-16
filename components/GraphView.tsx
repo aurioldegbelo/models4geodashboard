16a89ae6bba2975fbd27e2aff1dc4d6168e6fe2d
SimonMeissner <simon.meissner1@gmx.net>
2023-08-16 12:42:42 +0200

    feat: add Label to YAxis

--- components/GraphView.tsx
+++ components/GraphView.tsx
@@ -4,6 +4,7 @@ import { useSelectedDatasetStore } from "@/store/selectedDatasetStore";
 import { DifferenceFeature, Feature, TransormedData } from "@/types/types";
 import {
 	CartesianGrid,
+	Label,
 	Line,
 	LineChart,
 	ResponsiveContainer,
@@ -18,7 +19,7 @@ interface Props {
 	features: Feature[] | DifferenceFeature[];
 	usedOnHighlightingView?: boolean;
 	usedOnDifferenceOnlyView?: boolean;
-	allowEscapeViewBox?: boolean
+	allowEscapeViewBox?: boolean;
 }
 
 export default function GraphView(props: Props) {
@@ -82,6 +83,42 @@ export default function GraphView(props: Props) {
 		}
 	};
 
+	const getLabelStringForYAxis = (): string => {
+		let labelString = "";
+		if (dataset == "roadnetworkdensity") {
+			if (
+				comparisonFeature1 &&
+				comparisonFeature2 &&
+				props.usedOnDifferenceOnlyView
+			) {
+				labelString = "difference in density in km/km²";
+			} else {
+				labelString = "density in km/km²";
+			}
+		} else if (dataset == "greenlandpercentage") {
+			if (
+				comparisonFeature1 &&
+				comparisonFeature2 &&
+				props.usedOnDifferenceOnlyView
+			) {
+				labelString = "difference in coverage in %";
+			} else {
+				labelString = "coverage in %";
+			}
+		} else if (dataset == "woodlandpercentage") {
+			if (
+				comparisonFeature1 &&
+				comparisonFeature2 &&
+				props.usedOnDifferenceOnlyView
+			) {
+				labelString = "difference in coverage in %";
+			} else {
+				labelString = "coverage in %";
+			}
+		}
+		return labelString;
+	};
+
 	return (
 		<div className="leaflet-control bg-white h-1/2 p-5 pb-10 w-full rounded-lg mx-auto">
 			<OnViewDatasetDescription />
@@ -92,10 +129,21 @@ export default function GraphView(props: Props) {
 				>
 					<CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
 					<XAxis dataKey="year" />
-					<YAxis />
+					<YAxis>
+						<Label
+							value={getLabelStringForYAxis()}
+							angle={-90}
+							position="outside"
+							fill="#676767"
+							fontSize={10}
+						/>
+					</YAxis>
 					<Tooltip
 						content={<GraphViewHoverTooltip />}
-						allowEscapeViewBox={{ x: props.allowEscapeViewBox, y: false }}
+						allowEscapeViewBox={{
+							x: props.allowEscapeViewBox,
+							y: false,
+						}}
 					/>
 					{lines(props.features)}
 				</LineChart>
